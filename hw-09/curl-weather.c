c54ae8717b11ac52e7ccebb19b9092764b6a4339
dasalam <dasalam@gmail.com>
2023-03-22 22:30:48 +0300

    Choose max len in strncpy

--- hw-09/curl-weather.c
+++ hw-09/curl-weather.c
@@ -6,6 +6,8 @@
 #include <curl/curl.h>
 #include "jsmn.h"
 
+#define MIN(a, b) (((size_t) (a)) >= ((size_t) (b)) ? ((size_t) (b)) : ((size_t) (a)))
+
 
 char JSON[256 * 1024];
 size_t JSON_location = 0;
@@ -23,7 +25,7 @@ struct weather {
 	char cityname[128];
 };
 
-int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
+static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 	if (tok->type == JSMN_STRING && (int)strlen(s) == tok->end - tok->start &&
 	    strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
 		return 1;
@@ -34,11 +36,11 @@ int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 
 
 //parse JSON to get weather and cityname (to check input)
-int parse_json(struct weather *wth)
+static int parse_json(struct weather *wth)
 {
 	jsmn_parser p;
 	jsmntok_t t[4096];
-	char tmp_str[128];
+	char tmp_str[256];
 
 	jsmn_init(&p);
 	int r = jsmn_parse(&p, JSON, strlen(JSON), t, 4096);
@@ -48,71 +50,117 @@ int parse_json(struct weather *wth)
 		
 		return -1;
 	}
+	printf("r = %d\n", r);
 	
 	for (int i = 0; i < 64; i++) {
-		//printf("%d - type = %d\t", i, t[i].type);
-
 		if (jsoneq(JSON, &t[i], "FeelsLikeC")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+			
 			wth->feelslike = atoi(tmp_str);
 		}
 		else if (jsoneq(JSON, &t[i], "humidity")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+
 			wth->humidity = atoi(tmp_str);
 		}
 		else if (jsoneq(JSON, &t[i], "precipMM")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+			
 			wth->precip = atof(tmp_str);
 		}
 		else if (jsoneq(JSON, &t[i], "pressureInches")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+
 			wth->pressure = (int) (atof(tmp_str) * 25.4);
 		}
 		else if (jsoneq(JSON, &t[i], "temp_C")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+			
 			wth->temperature = atoi(tmp_str);
 		}
 		else if (jsoneq(JSON, &t[i], "windspeedKmph")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+
 			wth->wind_speed = atoi(tmp_str);
 		}
 		else if (jsoneq(JSON, &t[i], "winddir16Point")) {
-			strncpy(wth->wind_dir, JSON + t[i+1].start,
-				(t[i+1].end - t[i+1].start) < 128 ? t[i+1].end - t[i+1].start : 128);
-			if ((t[i+4].end - t[i+1].start) < 128) {
-				wth->wind_dir[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(wth->wind_dir, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(wth->wind_dir)));	
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(wth->wind_dir)) == sizeof(wth->wind_dir)) {
+				wth->wind_dir[sizeof(wth->wind_dir) - 1] = '\0';
 			}
 			else {
-				wth->wind_dir[127] = '\0';
+				wth->wind_dir[t[i+1].end - t[i+1].start] = '\0';
 			}
 		}
 		else if (jsoneq(JSON, &t[i], "areaName")) {
-			strncpy(wth->cityname, JSON + t[i+4].start,
-				(t[i+4].end - t[i+4].start) < 128 ? t[i+4].end - t[i+4].start : 128);
-			if ((t[i+4].end - t[i+4].start) < 128) {
-				wth->cityname[t[i+4].end - t[i+4].start] = '\0';
+			strncpy(wth->cityname, JSON + t[i+4].start, MIN(t[i+4].end - t[i+4].start, sizeof(wth->cityname)));
+			if (MIN(t[i+4].end - t[i+4].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				wth->cityname[sizeof(wth->cityname) - 1] = '\0';
 			}
 			else {
-				wth->cityname[127] = '\0';
+				wth->cityname[t[i+4].end - t[i+4].start] = '\0';
 			}
+			
 		}
 	}
 
 	for (int i = r; i > r-20; i--) {
 		if (jsoneq(JSON, &t[i], "maxtempC")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+
 			wth->max_temperature = atoi(tmp_str);
 		}
 		else if (jsoneq(JSON, &t[i], "mintempC")) {
-			strncpy(tmp_str, JSON + t[i+1].start, t[i+1].end - t[i+1].start);
-			tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			strncpy(tmp_str, JSON + t[i+1].start, MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)));
+			if (MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) == sizeof(tmp_str)) {
+				tmp_str[MIN(t[i+1].end - t[i+1].start, sizeof(tmp_str)) - 1] = '\0';
+			}
+			else {
+				tmp_str[t[i+1].end - t[i+1].start] = '\0';
+			}
+
 			wth->min_temperature = atoi(tmp_str);
 		}
 	}
@@ -120,7 +168,7 @@ int parse_json(struct weather *wth)
 	return 0;
 }
 
-size_t write_cb(char *ptr, size_t size, size_t nmemb, void *userdata)
+static size_t write_cb(char *ptr, size_t size, size_t nmemb, void *userdata)
 {	
 	strncpy(JSON + JSON_location, ptr, size * nmemb);
 	JSON_location += size * nmemb;
@@ -128,7 +176,7 @@ size_t write_cb(char *ptr, size_t size, size_t nmemb, void *userdata)
 	return size * nmemb;
 }
 
-void print_weather(struct weather *wth)
+static void print_weather(struct weather *wth)
 {
 	setlocale(LC_CTYPE, "");
 	wint_t deg = 0x00b0;
